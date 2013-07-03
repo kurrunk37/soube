3c95f98218ce42ffacd01f4ac2652ca2df6d8fe3
kurrunk <huzhengquan@baidu.com>
2013-07-03 20:24:02 +0800

    rm test

--- project.clj
+++ project.clj
@@ -3,7 +3,18 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
-                 [hiccup "1.0.3"]]
+;                 [hiccup "1.0.3"]
+;                 [enlive "1.1.1"]
+								 [de.ubercode.clostache/clostache "1.3.1"]
+;								 [clj-dropbox "1.2.1"]
+								 [jopbox "0.1.0-SNAPSHOT"]
+;                 [sandbar/sandbar "0.4.0-SNAPSHOT"]
+								 ;[clj-oauth "1.4.0"]
+;								 [clj-oauth "1.3.1-SNAPSHOT"]
+;								 [clj-http "0.7.2"]
+;								 [org.clojars.tavisrudd/clj-apache-http "2.3.2-SNAPSHOT"]
+
+								 ]
   :plugins [[lein-ring "0.8.3"]]
   :ring {:handler soube.handler/app}
   :profiles
--- src/soube/handler.clj
+++ src/soube/handler.clj
@@ -1,12 +1,78 @@
 (ns soube.handler
-  (:use compojure.core)
+  (:use [compojure.core]
+				[ring.util.response :only [redirect response]]
+				[ring.middleware session params keyword-params])
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+						[jopbox.client :as dropbox]
+            [clostache.parser :as clostache]
+						[soube.config :as config]))
+
+(defn render-template [template-file params]
+	(clostache/render 
+		(slurp 
+			(clojure.java.io/resource 
+				(str "templates/" template-file ".mustache")))
+		params))
+
+(defn index []
+  (render-template "index" {:name "is正全"}))
+
+(defn admin-index []
+  (render-template "admin" {:name "one admin"}))
+
+(defn post [id]
+	(str "hello" id))
+
+(defn authenticated
+  [{session :session}]
+  (boolean (:auth-token session)))
+
+(defn authenticate
+	[req]
+  (let [{session :session}     req
+        {port    :server-port} req
+        {hostname  :server-name} req
+        {path :uri} req
+        {scheme  :scheme}      req
+        {params  :params}      req
+        {token :oauth_token uid :uid error :oauth_problem} params
+        ;{token :oauth_token verifier :oauth_verifier error :oauth_problem} params
+        url (str (name scheme) "://" hostname (if (= 80 port) "" (str ":" port)) "/admin")]
+    (cond
+      error
+        (response (str "Authentication error " error))
+      (and token uid)
+        (do
+          (-> (redirect url)
+            (assoc :session (assoc session :auth-token token))))
+      :else
+				(let [request-token (dropbox/fetch-request-token config/consumer)
+					dropboxurl (dropbox/authorization-url config/consumer request-token)]
+          (redirect (str dropboxurl "&oauth_callback=" url))))))
+
+;; Authentication handler
+;; 验证的逻辑
+(defn wrap-auth
+	[handler]
+	(fn [req]
+		(if (and (.startsWith (:uri req) "/admin") (not (authenticated req)))
+          (authenticate req)
+          (handler req))))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (index))
+  (GET "/admin" [] (admin-index))
+  (GET ["/:id", :id  #"[0-9]+"] [id] (post id))
   (route/resources "/")
   (route/not-found "Not Found"))
 
+;(def app
+;  (handler/site app-routes))
+
 (def app
-  (handler/site app-routes))
+	(-> app-routes
+		wrap-auth
+		wrap-session
+		wrap-keyword-params
+		wrap-params))
